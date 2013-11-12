d65d52cddc17e6948c0025299e5cbb6a6507625a
Adam Schwartz <aschwartz@hubspot.com>
2013-11-11 20:15:17 -0500

    Rebuild docs

--- resources/executr/build/js/executr-run.js
+++ resources/executr/build/js/executr-run.js
@@ -1,12 +1,27 @@
 $(function(){
+    var codeSelector, switcherHTML, $docContent;
+
+    codeSelector = 'pre code[data-type="coffeescript"], pre code[data-type="javascript"]';
+
+    switcherHTML = '' +
+        '<ul class="executr-switch-outer">' +
+            '<li class="executr-switch" data-code-type="javascript">JS</li>' +
+            '<li class="executr-switch selected" data-code-type="coffeescript">Coffee</li>' +
+        '</ul>';
+
     $('pre code').each(function(){
         var $code = $(this),
             lang = $code.get(0).className.substr('lang-'.length)
         ;
         $code.attr('data-type', lang);
     });
+
+    if ($(codeSelector).length) {
+        $docContent = $(codeSelector).first().parents('.hs-doc-content').first().append(switcherHTML);
+    }
+
     $('body').executr({
-        codeSelector: 'pre code[data-type="coffeescript"], pre code[data-type="javascript"]',
+        codeSelector: codeSelector,
         codeMirrorOptions: {
             viewportMargin: Infinity
         }
