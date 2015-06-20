ef949a0d46e49f9a151442279d901e6386b1ede7
Nick Hwang <nhwang@hubspot.com>
2015-06-19 21:40:25 -0400

    Bring in build file and update *.json

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var del         = require('del');
+var gulp        = require('gulp');
+var babel       = require('gulp-babel');
+var bump        = require('gulp-bump');
+var header      = require('gulp-header');
+var minify      = require('gulp-minify-css');
+var prefixer    = require('gulp-autoprefixer');
+var rename      = require('gulp-rename');
+var uglify      = require('gulp-uglify');
+var sass        = require('gulp-sass');
+var umd         = require('gulp-wrap-umd');
+
+// Variables
+var distDir = './dist';
+var pkg = require('./package.json');
+var banner = ['/*!', pkg.name, pkg.version, '*/\n'].join(' ');
+var umdOptions = {
+  exports: 'Pace',
+  namespace: 'Pace',
+};
+
+
+// Clean
+gulp.task('clean', function() {
+  del.sync([distDir]);
+});
+
+
+// Javascript
+gulp.task('js', function() {
+  gulp.src('./src/js/drop.js')
+    .pipe(babel())
+    .pipe(umd(umdOptions))
+    .pipe(header(banner))
+
+    // Original
+    .pipe(gulp.dest(distDir + '/js'))
+
+    // Minified
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(distDir + '/js'));
+});
+
+
+// CSS
+gulp.task('css', function() {
+  gulp.src('./src/css/**/*.sass')
+    .pipe(sass({
+      includePaths: ['./bower_components']
+    }))
+    .pipe(prefixer())
+
+    // Original
+    .pipe(gulp.dest(distDir + '/css'))
+
+    // Minified
+    .pipe(minify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(distDir + '/css'));
+});
+
+
+// Version bump
+var VERSIONS = ['patch', 'minor', 'major'];
+for (var i = 0; i < VERSIONS.length; ++i){
+  (function(version) {
+    gulp.task('version:' + version, function() {
+      gulp.src(['package.json', 'bower.json'])
+        .pipe(bump({type: version}))
+        .pipe(gulp.dest('.'));
+    });
+  })(VERSIONS[i]);
+}
+
+
+// Watch
+gulp.task('watch', ['js', 'css'], function() {
+  gulp.watch('./src/js/**/*', ['js']);
+  gulp.watch('./src/css/**/*', ['css']);
+});
+
+
+// Defaults
+gulp.task('build', ['js', 'css']);
+gulp.task('default', ['build']);
+
+
