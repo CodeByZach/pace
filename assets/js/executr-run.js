12bf044cf516f9d24fc63063cd0779553dc5c0d1
Zachary Miller <codebyzach@gmail.com>
2021-01-21 16:42:23 -0500

    Restructuring

--- assets/js/executr-run.js
+++ assets/js/executr-run.js
@@ -1,34 +1,34 @@
 $(function(){
-    var codeSelector, switcherHTML, $docContent, options;
+	var codeSelector, switcherHTML, $docContent, options;
 
-    codeSelector = 'pre code[data-type="coffeescript"], pre code[data-type="javascript"]';
+	codeSelector = 'pre code[data-type="coffeescript"], pre code[data-type="javascript"]';
 
-    switcherHTML = '' +
-        '<ul class="executr-switch-outer">' +
-            '<li class="executr-switch" data-code-type="javascript">JS</li>' +
-            '<li class="executr-switch selected" data-code-type="coffeescript">Coffee</li>' +
-        '</ul>';
+	switcherHTML = '' +
+		'<ul class="executr-switch-outer">' +
+			'<li class="executr-switch" data-code-type="javascript">JS</li>' +
+			'<li class="executr-switch selected" data-code-type="coffeescript">Coffee</li>' +
+		'</ul>';
 
-    $('pre code').each(function(){
-        var $code = $(this),
-            lang = $code.get(0).className.substr('lang-'.length)
-        ;
-        $code.attr('data-type', lang);
-    });
+	$('pre code').each(function(){
+		var $code = $(this),
+			lang = $code.get(0).className.substr('lang-'.length)
+		;
+		$code.attr('data-type', lang);
+	});
 
-    if ($(codeSelector).length) {
-        $docContent = $(codeSelector).first().parents('.hs-doc-content').first().append(switcherHTML);
-    }
+	if ($(codeSelector).length) {
+		$docContent = $(codeSelector).first().parents('.hs-doc-content').first().append(switcherHTML);
+	}
 
-    options = {
-        codeSelector: codeSelector,
-        codeMirrorOptions: {
-            viewportMargin: Infinity
-        }
-    };
+	options = {
+		codeSelector: codeSelector,
+		codeMirrorOptions: {
+			viewportMargin: Infinity
+		}
+	};
 
-    if (window.EXECUTR_OPTIONS)
-        $.extend(true, options, EXECUTR_OPTIONS);
+	if (window.EXECUTR_OPTIONS)
+		$.extend(true, options, EXECUTR_OPTIONS);
 
-    $('body').executr(options);
+	$('body').executr(options);
 });
--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,44 +1,44 @@
 (function() {
-  var Color, compileTheme, loadTheme, vm;
+	var Color, compileTheme, loadTheme, vm;
 
-  if (typeof module !== "undefined" && module !== null) {
-    vm = require('vm');
-    Color = require('color');
-  }
+	if (typeof module !== "undefined" && module !== null) {
+		vm = require('vm');
+		Color = require('color');
+	}
 
-  loadTheme = function(name, cb) {
-    return $.ajax({
-      url: "/pace/assets/css/templates/pace-theme-" + name + ".tmpl.css",
-      success: cb
-    });
-  };
+	loadTheme = function(name, cb) {
+		return $.ajax({
+			url: "/pace/assets/css/templates/pace-theme-" + name + ".tmpl.css",
+			success: cb
+		});
+	};
 
-  compileTheme = function(body, args) {
-    if (args == null) {
-      args = {};
-    }
-    return body.replace(/`([\s\S]*?)`/gm, function(match, code) {
-      var val;
-      if (typeof module !== "undefined" && module !== null) {
-        val = vm.runInNewContext(code, {
-          args: args,
-          Color: Color
-        });
-      } else {
-        Color = window.Color;
-        val = eval(code);
-      }
-      return val;
-    });
-  };
+	compileTheme = function(body, args) {
+		if (args == null) {
+			args = {};
+		}
+		return body.replace(/`([\s\S]*?)`/gm, function(match, code) {
+			var val;
+			if (typeof module !== "undefined" && module !== null) {
+				val = vm.runInNewContext(code, {
+					args: args,
+					Color: Color
+				});
+			} else {
+				Color = window.Color;
+				val = eval(code);
+			}
+			return val;
+		});
+	};
 
-  if (typeof module !== "undefined" && module !== null) {
-    module.exports = {
-      compileTheme: compileTheme
-    };
-  } else {
-    window.loadTheme = loadTheme;
-    window.compileTheme = compileTheme;
-  }
+	if (typeof module !== "undefined" && module !== null) {
+		module.exports = {
+			compileTheme: compileTheme
+		};
+	} else {
+		window.loadTheme = loadTheme;
+		window.compileTheme = compileTheme;
+	}
 
 }).call(this);
